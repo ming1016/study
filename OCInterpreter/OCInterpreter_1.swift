00ff8ea49d6369b0679f5069dc65a4a612b7eb73
ming1016 <ming1016@foxmail.com>
2018-06-09 15:18:07 +0800

    demo

--- OCInterpreter/OCInterpreter_1.swift
+++ OCInterpreter/OCInterpreter_1.swift
@@ -16,12 +16,16 @@ public enum OCConstant {
 
 public enum OCOperation {
     case plus
+    case minus
+    case mult
+    case intDiv
 }
 
 public enum OCToken {
     case constant(OCConstant)
     case operation(OCOperation)
     case eof
+    case whiteSpaceAndNewLine
 }
 
 extension OCConstant: Equatable {
@@ -46,6 +50,14 @@ extension OCOperation: Equatable {
         switch (lhs, rhs) {
         case (.plus, .plus):
             return true
+        case (.minus, .minus):
+            return true
+        case (.mult, .mult):
+            return true
+        case (.intDiv, .intDiv):
+            return true
+        default:
+            return false
         }
     }
 }
@@ -59,6 +71,8 @@ extension OCToken: Equatable {
             return left == right
         case (.eof, .eof):
             return true
+        case (.whiteSpaceAndNewLine, .whiteSpaceAndNewLine):
+            return true
         default:
             return false
         }
@@ -68,7 +82,7 @@ extension OCToken: Equatable {
 public class OCInterpreter {
     private let text: String
     private var currentIndex: Int
-    private var currentCharacter: Character
+    private var currentCharacter: Character?
     
     private var currentTk: OCToken
     
@@ -88,15 +102,30 @@ public class OCInterpreter {
             return .eof
         }
         
-        if CharacterSet.decimalDigits.contains(currentCharacter.unicodeScalars.first!) {
-            let tk = OCToken.constant(.integer(Int(String(currentCharacter))!))
-            advance()
-            return tk
+        if CharacterSet.whitespacesAndNewlines.contains((currentCharacter?.unicodeScalars.first!)!) {
+            skipWhiteSpaceAndNewLines()
+            return .whiteSpaceAndNewLine
+        }
+        
+        if CharacterSet.decimalDigits.contains((currentCharacter?.unicodeScalars.first!)!) {
+            return number()
         }
         
         if currentCharacter == "+" {
             advance()
             return .operation(.plus)
+        }
+        if currentCharacter == "-" {
+            advance()
+            return .operation(.minus)
+        }
+        if currentCharacter == "*" {
+            advance()
+            return .operation(.mult)
+        }
+        if currentCharacter == "/" {
+            advance()
+            return .operation(.intDiv)
         }
         advance()
         return .eof
@@ -110,28 +139,68 @@ public class OCInterpreter {
         }
         eat(currentTk)
         
-        eat(.operation(.plus))
+        let op = currentTk
+        eat(currentTk)
         
         guard case let .constant(.integer(right)) = currentTk else {
             return 0
         }
         eat(currentTk)
         
+        if op == .operation(.plus) {
+            return left + right
+        } else if op == .operation(.minus) {
+            return left - right
+        } else if op == .operation(.mult) {
+            return left * right
+        } else if op == .operation(.intDiv) {
+            return left / right
+        }
         return left + right
     }
     
+    // 数字处理
+    private func number() -> OCToken {
+        var numStr = ""
+        while let character = currentCharacter,  CharacterSet.decimalDigits.contains((character.unicodeScalars.first!)) {
+            numStr += String(character)
+            advance()
+        }
+        
+        return .constant(.integer(Int(numStr)!))
+    }
+    
     // 辅助函数
-    func advance() {
+    private func advance() {
         currentIndex += 1
         guard currentIndex < text.count else {
+            currentCharacter = nil
             return
         }
         currentCharacter = text[text.index(text.startIndex, offsetBy: currentIndex)]
     }
     
-    func eat(_ token: OCToken) {
+    // 往前探一个字符，不改变当前字符
+    private func peek() -> Character? {
+        let peekIndex = currentIndex + 1
+        guard peekIndex < text.count else {
+            return nil
+        }
+        return text[text.index(text.startIndex, offsetBy: peekIndex)]
+    }
+    
+    private func skipWhiteSpaceAndNewLines() {
+        while let character = currentCharacter, CharacterSet.whitespacesAndNewlines.contains((character.unicodeScalars.first!)) {
+            advance()
+        }
+    }
+    
+    private func eat(_ token: OCToken) {
         if  currentTk == token {
             currentTk = nextTk()
+            if currentTk == OCToken.whiteSpaceAndNewLine {
+                currentTk = nextTk()
+            }
         } else {
             error()
         }
